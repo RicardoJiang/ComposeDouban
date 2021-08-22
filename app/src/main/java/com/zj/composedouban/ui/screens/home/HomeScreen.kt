120002959aba3df0d61acb23cf8e7bc53795dbc6
shenzhen2017 <2868405029@qq.com>
2021-08-22 17:26:38 +0800

    ADD: 年度榜单

--- app/src/main/java/com/zj/composedouban/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/zj/composedouban/ui/screens/home/HomeScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
@@ -19,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import com.google.accompanist.coil.rememberCoilPainter
 import com.google.accompanist.insets.statusBarsHeight
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.zj.composedouban.util.noRippleClickable
 
 @Composable
 fun HomeScreen() {
@@ -29,19 +32,26 @@ fun HomeScreen() {
                 .fillMaxWidth()
                 .statusBarsHeight()
         )
-        Text(
-            text = "电影榜单",
-            color = Color.Black,
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 8.dp)
-        )
-        LazyRow {
-            items(3) {
-                TopRankItem()
-            }
+        TopRank()
+        YearRank()
+    }
+}
+
+@Composable
+fun TopRank() {
+    Text(
+        text = "电影榜单",
+        color = Color.Black,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 8.dp)
+    )
+    LazyRow {
+        items(3) {
+            TopRankItem()
         }
     }
 }
+
 
 @Composable
 fun TopRankItem() {
@@ -117,6 +127,80 @@ fun TopRankItem() {
                 style = MaterialTheme.typography.overline,
                 modifier = Modifier.padding(12.dp, 0.dp, 0.dp, 2.dp)
             )
+        }
+    }
+}
+
+@Composable
+fun YearRank() {
+    Text(
+        text = "年度电影榜单",
+        color = Color.Black,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier.padding(16.dp, 8.dp)
+    )
+    var selectIdx by remember { mutableStateOf(0) }
+    val list = listOf("2020", "2019", "2018", "2017", "2016", "2015", "2014")
+    YearRow(list = list, selectIdx = selectIdx) {
+        selectIdx = it
+    }
+    YearRankItems(list[selectIdx])
+}
+
+@Composable
+fun YearRow(list: List<String>, selectIdx: Int, onSelectChange: (Int) -> Unit) {
+    LazyRow(contentPadding = PaddingValues(4.dp, 8.dp, 8.dp, 0.dp)) {
+        itemsIndexed(list) { index, it ->
+            val backgroundColor = if (index == selectIdx) 0xFFE0F7E8 else 0xFFF6F6F6
+            val contentColor = if (index == selectIdx) 0xFF008E00 else 0xFF000000
+            Text(
+                text = it,
+                color = Color(contentColor),
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier
+                    .padding(8.dp, 0.dp, 0.dp, 0.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(Color(backgroundColor))
+                    .padding(7.dp, 4.dp)
+                    .noRippleClickable {
+                        onSelectChange.invoke(index)
+                    }
+            )
+        }
+    }
+}
+
+@Composable
+fun YearRankItems(year: String) {
+    val titleList = listOf("华语电影", "外语电影", "冷门佳片")
+    val colorList = listOf(0xff442526, 0xff424243, 0xff442C2F)
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 16.dp, 0.dp, 0.dp)
+    ) {
+        for (i in titleList.indices) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color(colorList[i]))
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = "${year}高分榜",
+                        color = Color.White,
+                        style = MaterialTheme.typography.body1
+                    )
+                    Text(
+                        text = titleList[i],
+                        color = Color.White,
+                        style = MaterialTheme.typography.h5
+                    )
+                }
+            }
         }
     }
 }
